57dd34d9ff8aba4c1d630583434679b7a5364b2d
DrLeeroyPhD <dennyjchristopher@gmail.com>
2018-10-28 14:23:01 -0700

    desktop and taskbar templating complete

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+var desktop = new Vue({
+  el: '#desktopContainer',
+  data: {
+    desktop: {
+      me: {
+        displayName: "Me",
+        imgLoc: "./assets/img/folder-icon.png",
+      },
+      about: {
+        displayName: "About",
+        imgLoc: "./assets/img/folder-icon.png"
+      },
+      Portfolio: {
+        displayName: "Portfolio",
+        imgLoc: "./assets/img/folder-icon.png"
+      },
+      Contact: {
+        displayName: "Contact",
+        imgLoc: "./assets/img/folder-icon.png"
+      }
+    }
+  }
+})

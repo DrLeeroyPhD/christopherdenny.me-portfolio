dbe6cfc079b6aad4131a789cd91c169a8194b030
DrLeeroyPhD <dennyjchristopher@gmail.com>
2018-10-30 14:29:52 -0700

    Modal created, resizable, needs further styling and made more dynamic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,65 @@
+Vue.component('modal-window', {
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+  },
+  template: `
+    <transition name="modal-fade">
+      <div class="modal"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+      >
+        <header
+          class="modal-header"
+          id="modalTitle"
+        >
+          <slot name="header">
+            This is the default tile!
+
+            <button
+              type="button"
+              class="btn-close"
+              @click="close"
+              aria-label="Close modal"
+            >
+              x
+            </button>
+          </slot>
+        </header>
+        <section
+          class="modal-body"
+          id="modalDescription"
+        >
+          <slot name="body">
+            I'm the default body!
+          </slot>
+        </section>
+        <footer class="modal-footer">
+          <slot name="footer">
+            I'm the default footer!
+          </slot>
+        </footer>
+      </div>
+    </transition>
+  `
+})
+
+
+
 var desktop = new Vue({
   el: '#desktopContainer',
   data: {
+    isModalVisible: false,
     desktop: {
       me: {
         displayName: "Me",
-        imgLoc: "./assets/img/folder-icon.png",
+        imgLoc: "./assets/img/file-icon.png",
       },
       about: {
         displayName: "About",
-        imgLoc: "./assets/img/folder-icon.png"
+        imgLoc: "./assets/img/file-icon.png"
       },
       Portfolio: {
         displayName: "Portfolio",
@@ -18,6 +69,14 @@ var desktop = new Vue({
         displayName: "Contact",
         imgLoc: "./assets/img/folder-icon.png"
       }
+    },
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
     }
   }
 })

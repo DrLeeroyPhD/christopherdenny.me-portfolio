d04db33231fe21749375346f7e88265a2fbb6b93
DrLeeroyPhD <dennyjchristopher@gmail.com>
2018-10-30 15:57:35 -0700

    Adjusted modal to start closer to the center and made all modal windows draggable once I can get multiple modals to work

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,19 @@
 /***************************************
               MODAL DRAGGING
 ***************************************/
-var draggie = new Draggabilly('.draggable');
+// if you have multiple .draggable elements
+// get all draggie elements
+var draggableElems = document.querySelectorAll('.draggable');
+// array of Draggabillies
+var draggies = []
+// init Draggabillies
+for ( var i=0; i < draggableElems.length; i++ ) {
+  var draggableElem = draggableElems[i];
+  var draggie = new Draggabilly( draggableElem, {
+    // options...
+  });
+  draggies.push( draggie );
+}
 
 
 

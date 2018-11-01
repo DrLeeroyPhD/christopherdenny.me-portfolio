185af8fe6c212660c565be89d04580957c829217
DrLeeroyPhD <dennyjchristopher@gmail.com>
2018-11-01 16:40:06 -0700

    Windows now open, close, resize, move, and stack properly

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,65 @@
               COMPONENTS
 ***************************************/
 Vue.component('modal-window', {
+
+  /***** METHODS *****/
   methods: {
     closeModal() {
       this.isModalVisible = false;
     },
+    focusLevel() {
+      console.log(this.levelOfFocus);
+    },
+    blurFocus() {
+      this.levelOfFocus = 2;
+    },
   },
+
+  /***** DATA *****/
   data() {
     return {
       activeTitle: "Folder",
       isModalVisible: true,
+      levelOfFocus: 2,
     }
   },
+
+  /***** DIRECTIVES *****/
+  directives: {
+    /*** Capture when user clicks away from an element (change z-index of windows) ***/
+    clickoutside: {
+      bind: function (el, binding, vnode) {
+        el.clickOutsideEvent = function (event) {
+          // Check that click was outside the el and its children
+          if (!(el == event.target || el.contains(event.target))) {
+            // and if so, call method provided in attribute value
+            vnode.context[binding.expression](event);
+          }
+        };
+        document.body.addEventListener('click', el.clickOutsideEvent)
+      },
+      unbind: function (el) {
+        document.body.removeEventListener('click', el.clickOutsideEvent)
+      },
+    },
+  },
+
+  /***** PROPS *****/
   props: ['content'],
+
+  /***** TEMPLATE *****/
   template: `
     <transition name="modal-fade">
-      <div class="modal draggable" role="dialog" aria-labelledby="modalTitle" v-show="isModalVisible" aria-describedby="modalDescription">
+      <div
+        class="modal draggable"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        v-show="isModalVisible"
+        aria-describedby="modalDescription"
+        :style="{zIndex: levelOfFocus}"
+        @click="levelOfFocus = 19;"
+        v-clickoutside="blurFocus"
+      >
         <header class="modal-header handle" id="modalTitle">
           <h3>
             <slot name="header">
@@ -89,5 +133,37 @@ var desktop = new Vue({
       open = clicked;
       console.log(open);
     },
-  }
+    makeDraggable(){
+      setTimeout(function () {
+        var draggableElems = document.querySelectorAll('.draggable');
+        // array of Draggabillies
+        var draggies = [];
+        // init Draggabillies
+        for ( var i=0; i < draggableElems.length; i++ ) {
+          var draggableElem = draggableElems[i];
+          var draggie = new Draggabilly( draggableElem, {
+            handle: '.handle'
+          });
+          draggies.push( draggie );
+        };
+    }, 200);
+
+
+    }
+  },
 });
+
+/** **/
+Vue.directive('click-outside', {
+  bind () {
+      this.event = event => this.vm.$emit(this.expression, event)
+      this.el.addEventListener('click', this.stopProp)
+      document.body.addEventListener('click', this.event)
+  },
+  unbind() {
+    this.el.removeEventListener('click', this.stopProp)
+    document.body.removeEventListener('click', this.event)
+  },
+
+  stopProp(event) { event.stopPropagation() }
+})

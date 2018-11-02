5dd0875182b8a9131cbd1cd176d382409415dab5
DrLeeroyPhD <dennyjchristopher@gmail.com>
2018-11-02 13:02:06 -0700

    Changed clock to am/pm format

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,12 +25,20 @@ function makeDraggable() {
 ***************************************/
 var currentDate = new Date();
 
+var amPM = "AM";
+
 var date = currentDate.getDate();
 var month = currentDate.getMonth();
 var year = currentDate.getFullYear();
 
 var hour = currentDate.getHours();
 var minute = currentDate.getMinutes();
+
+// Determine if AM or PM and format by 12-hour clock
+if(hour > 12) {
+  hour = hour-12;
+  amPM = "PM";
+}
 
 // Help format time to turn things like 12:7 -> 12:07
 if(minute < 10){
@@ -39,7 +47,7 @@ if(minute < 10){
   minute = "00";
 }
 
-var timeString = hour + ":" + minute;
+var timeString = hour + ":" + minute + " " + amPM;
 var dateString = month + "/" +(date + 1) + "/" + year;
 
 document.getElementById("time").innerHTML = timeString;
